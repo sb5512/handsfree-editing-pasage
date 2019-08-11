80c6bc39249364635efad2af8107e7568847627f
Sabin Bhattarai <sabin.bhattarai@hotmail.com>
2019-08-11 18:11:21 +0200

    design multimodal

--- src/components/homePage/multimodal.jsx
+++ src/components/homePage/multimodal.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import keydown from "react-keydown";
+import Row from "react-bootstrap/Row";
 
 class Multimodal extends Component {
   componentWillReceiveProps({ keydown }) {
@@ -36,38 +37,64 @@ class Multimodal extends Component {
           <i className="fa fa-chevron-up">(b)</i>
         </button> */}
 
-        <div className="row">
+        <Row>
           <button className="btn btn-light btn-lg float-right text-center">
             <i className="fa fa-microphone" />
             <br />
             <i className="fa fa-eye" />
           </button>
-        </div>
-        <div className="row">
-          <div className="col-3">
-            <Link to="/copytaskcommand">
-              <button type="button" className="btn btn-success btn-block ">
-                <br />
-                <br />
-                <h2>Copy Task Command Tag</h2>
-                <br />
-                <br />
-              </button>
-            </Link>
+        </Row>
+        <Row>
+          <div className="col-6">
+            <Row>
+              <p className="text-center w-100">
+                <h2>Command TAG</h2>
+              </p>
+            </Row>
           </div>
-          <div className="col-3">
-            <Link to="/copytaskdwell">
-              <button type="button" className="btn btn-success btn-block">
-                <br />
-                <br />
-                <h2>Copy Task Dwell Tag</h2>
-                <br />
-                <br />
-              </button>
-            </Link>
+          <div className="col-6">
+            <Row>
+              <p className="text-center w-100">
+                <h2>Dwell TAG</h2>
+              </p>
+            </Row>
           </div>
+          <Row>
+            <div className="col-3">
+              <Link to="/copytaskcommand">
+                <button type="button" className="btn btn-success btn-block ">
+                  <br />
+                  <br />
+                  <h2>Copy Task</h2>
+                  <br />
+                  <br />
+                </button>
+              </Link>
+            </div>
+            <div className="col-3">
+              <Link to="/freetextformationtaskcommand">
+                <button type="button" className="btn btn-primary btn-block">
+                  <br />
+                  <br />
+                  <h2>Free Text formation</h2>
+                  <br />
+                </button>
+              </Link>
+            </div>
 
-          {/* <div className="col-3">
+            <div className="col-3">
+              <Link to="/copytaskdwell">
+                <button type="button" className="btn btn-success btn-block">
+                  <br />
+                  <br />
+                  <h2>Copy Task</h2>
+                  <br />
+                  <br />
+                </button>
+              </Link>
+            </div>
+
+            {/* <div className="col-3">
             <Link to="/replytask">
               <button type="button" className="btn btn-primary btn-block">
                 <br />
@@ -78,27 +105,19 @@ class Multimodal extends Component {
               </button>
             </Link>
           </div> */}
-          <div className="col-3">
-            <Link to="/freetextformationtaskcommand">
-              <button type="button" className="btn btn-primary btn-block">
-                <br />
-                <br />
-                <h2>Free Text formation Command </h2>
-                <br />
-              </button>
-            </Link>
-          </div>
-          <div className="col-3">
-            <Link to="/freetextformationtaskdwell">
-              <button type="button" className="btn btn-primary btn-block">
-                <br />
-                <br />
-                <h2>Free Text Formation Dwell</h2>
-                <br />
-              </button>
-            </Link>
-          </div>
-        </div>
+
+            <div className="col-3">
+              <Link to="/freetextformationtaskdwell">
+                <button type="button" className="btn btn-primary btn-block">
+                  <br />
+                  <br />
+                  <h2>Free Text Formation</h2>
+                  <br />
+                </button>
+              </Link>
+            </div>
+          </Row>
+        </Row>
       </div>
     );
   }
